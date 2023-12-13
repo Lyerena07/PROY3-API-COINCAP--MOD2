70f54c6ebde0df90c9ba4a9dd3054327f2f6262f
Jorge Rea <danireaprado@hotmail.com>
2023-12-13 17:14:18 -0600

    Diseño mejorado

--- js/graficaTopFive.js
+++ js/graficaTopFive.js
@@ -1,14 +1,14 @@
 const myChart_Top5 = document.getElementById("myChart_Top5")
 
-const pintarGrafica = () => {
-   new Chart(myChart_Top5, {
+const topFiveGraph = () => {
+  new Chart(myChart_Top5, {
         type: "line", // line, pie, bar, radar, doughnut
         data: {
-          labels:["Bitcoin","Ethereum", "Tether","BNB", "Solana"], // eje x
+          labels:["Bitcoin","Ethereum", "Tether","BNB", "Solana"],
           datasets: [
             {
-              label: "Grafica Top5",
-              data:[41, 20, 14, 54, 67], // eje y
+              label: "",
+              data:[41, 20, 14, 54, 67],
               borderWidth: 1,
               backgroundColor: ["#ff6384", "#36a2eb", "yellow", "green"],
             },
@@ -25,4 +25,4 @@ const pintarGrafica = () => {
     };
     
 
-window.addEventListener("load", pintarGrafica);
+window.addEventListener("load", topFiveGraph);
