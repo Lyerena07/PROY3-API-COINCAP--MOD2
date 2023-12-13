18266a568e8cc795aab8216ad8770ee061c49cee
Linda Lucia Yerena Perez <lucyyerens@gmail.com>
2023-12-12 20:50:48 -0500

    Adiccion de Grafica top 5, en HTML,CSS Y JS

--- js/index.js
+++ js/index.js
@@ -1,5 +1,28 @@
+const myChart_Top5 = document.getElementById("myChart_Top5")
 
-const  = document.getElementById("")
-const  = document.getElementById("")
-let = document.getElementById('')
+const pintarGrafica = () => {
+   new Chart(myChart_Top5, {
+        type: "line", // line, pie, bar, radar, doughnut
+        data: {
+          labels:["Bitcoin","Ethereum", "Tether","BNB", "Solana"], // eje x
+          datasets: [
+            {
+              label: "Grafica Top5",
+              data:[41, 20, 14, 54, 67], // eje y
+              borderWidth: 1,
+              backgroundColor: ["#ff6384", "#36a2eb", "yellow", "green"],
+            },
+          ],
+        },
+        options: {
+          scales: {
+            y: {
+              beginAtZero: true,
+            },
+          },
+        },
+      });
+    };
+    
 
+window.addEventListener("load", pintarGrafica);
