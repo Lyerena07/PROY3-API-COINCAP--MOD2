6fd77ae791d290bb394b48b32562b32a510da71b
Jorge Rea <danireaprado@hotmail.com>
2023-12-15 15:05:46 -0600

    Gráfica top 5 conectada a API

--- js/graphs/graficaTopFive.js
+++ js/graphs/graficaTopFive.js
@@ -1,28 +1,54 @@
-const myChart_Top5 = document.getElementById("myChart_Top5")
+const myChart_Top5 = document.getElementById("myChart_Top5");
 
-const topFiveGraph = () => {
+// Función para realizar una petición a la API y obtener los datos
+const fetchTopCryptoByPrice = async () => {
+  try {
+    const response = await fetch('https://api.coincap.io/v2/assets');
+    const data = await response.json();
+    // Ordenamos los resultados por precio de mayor a menor
+    const sortedByPrice = data.data.sort((a, b) => parseFloat(b.priceUsd) - parseFloat(a.priceUsd));
+    return sortedByPrice.slice(0, 5); // Obtenemos solo el top 5 por precio
+  } catch (error) {
+    console.error("Error al obtener los datos de la API", error);
+    return [];
+  }
+};
+
+const topFiveGraph = async () => {
+  const topFiveData = await fetchTopCryptoByPrice();
+
+  // Procesamos los datos para la gráfica
+  const labels = topFiveData.map(coin => coin.id); // Usamos el ID como etiqueta
+  const priceData = topFiveData.map(coin => parseFloat(coin.priceUsd)); // Obtenemos el precio
+
   new Chart(myChart_Top5, {
-        type: "line", // line, pie, bar, radar, doughnut
-        data: {
-          labels:["Bitcoin","Ethereum", "Tether","BNB", "Solana"],
-          datasets: [
-            {
-              label: "",
-              data:[41, 20, 14, 54, 67],
-              borderWidth: 1,
-              backgroundColor: ["#ff6384", "#36a2eb", "yellow", "green"],
-            },
-          ],
+    type: "bar",
+    data: {
+      labels: labels,
+      datasets: [
+        {
+          label: "Precio (USD)",
+          data: priceData,
+          borderWidth: 1,
+          backgroundColor: ["#ff6384", "#36a2eb", "yellow", "green", "#551a8b"],
         },
-        options: {
-          scales: {
-            y: {
-              beginAtZero: true,
-            },
-          },
+      ],
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: false,
         },
-      });
-    };
-    
+      },
+      tooltips: {
+        callbacks: {
+          label: function(tooltipItem, data) {
+            return `${data.datasets[tooltipItem.datasetIndex].label}: $${tooltipItem.yLabel.toLocaleString()}`;
+          }
+        }
+      }
+    },
+  });
+};
 
 window.addEventListener("load", topFiveGraph);
